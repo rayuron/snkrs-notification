271915b4e70a75d8a118fcbab2e79c46eac8c472
rayuron <rayuron@me.com>
2022-05-26 00:28:16 +0900

    fix bug

--- scrape.sh
+++ scrape.sh
@@ -11,8 +11,10 @@ echo "::set-output name=changed::false"
 else 
 echo `xmllint --html --xpath "//div[@data-qa='product-card-0']//@aria-label" out.html | sed 's/^.*"\(.*\)".*$/\1/'` > info.txt
 echo https://www.nike.com/jp/launch/`xmllint --html --xpath "//div[@data-qa='product-card-0']//@href" out.html | sed 's/^.*"\(.*\)".*$/\1/'` >> info.txt
+title=`head -n 1 info.txt`
+url=`tail -n 1 info.txt`
 
 echo "::set-output name=changed::true"
-echo "::set-output name=title::$(echo head -n 1 info.txt)"
-echo "::set-output name=url::$(echo tail -n 1 info.txt)"
+echo "::set-output name=title::$(echo $title)"
+echo "::set-output name=url::$(echo $url)"
 fi
