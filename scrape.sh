8c2f290a5e05b2c0fb5433282a0a332a7377616b
rayuron <rayuron@me.com>
2022-05-27 18:14:32 +0900

    update url

--- scrape.sh
+++ scrape.sh
@@ -1,19 +1,18 @@
 curl -L https://www.nike.com/jp/launch > out.html
 sed -i "s/<head>/'<head><meta charset=utf-8 \/>/g" out.html
 
-pre_url=`tail -n 1 info.txt`
-cat $pre_url
-post_url=https://www.nike.com/jp/launch/`xmllint --html --xpath "//div[@data-qa='product-card-0']//@href" out.html | sed 's/^.*"\(.*\)".*$/\1/'`
-cat $post_url
+pre_url=`head -n 1 info.txt`
+cat $pre_title
+post_url=`xmllint --html --xpath "//div[@data-qa='product-card-0']//@aria-label" out.html | sed 's/^.*"\(.*\)".*$/\1/'`
+cat $post_title
 
-if [ $pre_url = $post_url ]; then
+if [ $pre_title = $post_title ]; then
 echo "::set-output name=changed::false"
 else 
 echo `xmllint --html --xpath "//div[@data-qa='product-card-0']//@aria-label" out.html | sed 's/^.*"\(.*\)".*$/\1/'` > info.txt
 echo https://www.nike.com/jp/launch/`xmllint --html --xpath "//div[@data-qa='product-card-0']//@href" out.html | sed 's/^.*"\(.*\)".*$/\1/'` >> info.txt
 title=`head -n 1 info.txt`
 url=`tail -n 1 info.txt`
-
 echo "::set-output name=changed::true"
 echo "::set-output name=title::$(echo $title)"
 echo "::set-output name=url::$(echo $url)"
