7cee38c725f4333d504c0bd9014f15fca76020da
rayuron <rayuron@me.com>
2022-06-04 16:40:05 +0900

    fix bug

--- scrape.sh
+++ scrape.sh
@@ -2,13 +2,13 @@ curl -L https://www.nike.com/jp/launch > out.html
 sed -i -e "s/<head>/<head><meta charset=utf-8 \/>/g" out.html
 
 pre_title=`head -n 1 info.txt`
-cat $pre_title
+echo $pre_title
 post_title=`xmllint --html --xpath "//div[@data-qa='product-card-0']//@aria-label" out.html | sed 's/^.*"\(.*\)".*$/\1/'`
-cat $post_title
+echo $post_title
 
 if [ $pre_title = $post_title ]; then
 echo "::set-output name=changed::false"
-else 
+else
 echo `xmllint --html --xpath "//div[@data-qa='product-card-0']//@aria-label" out.html | sed 's/^.*"\(.*\)".*$/\1/'` > info.txt
 echo https://www.nike.com/jp/launch/`xmllint --html --xpath "//div[@data-qa='product-card-0']//@href" out.html | sed 's/^.*"\(.*\)".*$/\1/'` >> info.txt
 title=`head -n 1 info.txt`
