eae10986e79849831f42350aa9a1f7c78ccb467f
rayuron <rayuron@me.com>
2022-06-04 17:15:25 +0900

    hide logs

--- scrape.sh
+++ scrape.sh
@@ -3,14 +3,14 @@ sed -i -e "s/<head>/<head><meta charset=utf-8 \/>/g" out.html
 
 pre_title=`head -n 1 info.txt`
 echo $pre_title
-post_title=`xmllint --html --xpath "//div[@data-qa='product-card-0']//@aria-label" out.html | sed 's/^.*"\(.*\)".*$/\1/'`
+post_title=`xmllint --html --xpath "//div[@data-qa='product-card-0']//@aria-label" out.html 2>/dev/null | sed 's/^.*"\(.*\)".*$/\1/'`
 echo $post_title
 
 if [ "$pre_title" = "$post_title" ]; then
 echo "::set-output name=changed::false"
 else
-echo `xmllint --html --xpath "//div[@data-qa='product-card-0']//@aria-label" out.html | sed 's/^.*"\(.*\)".*$/\1/'` > info.txt
-echo https://www.nike.com/jp/launch/`xmllint --html --xpath "//div[@data-qa='product-card-0']//@href" out.html | sed 's/^.*"\(.*\)".*$/\1/'` >> info.txt
+echo `xmllint --html --xpath "//div[@data-qa='product-card-0']//@aria-label" out.html 2>/dev/null | sed 's/^.*"\(.*\)".*$/\1/'` > info.txt
+echo https://www.nike.com/jp/launch/`xmllint --html --xpath "//div[@data-qa='product-card-0']//@href" out.html 2>/dev/null | sed 's/^.*"\(.*\)".*$/\1/'` >> info.txt
 title=`head -n 1 info.txt`
 url=`tail -n 1 info.txt`
 echo "::set-output name=changed::true"
