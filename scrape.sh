3fca6e0539a8c44b633409aef04220a49d3146b4
rayuron <rayuron@me.com>
2022-06-06 22:24:57 +0900

    update code

--- scrape.sh
+++ scrape.sh
@@ -1,12 +1,12 @@
 curl -L https://www.nike.com/jp/launch > out.html
 sed -i -e "s/<head>/<head><meta charset=utf-8 \/>/g" out.html
 
-pre_title=`head -n 1 info.txt`
+pre_title=`head -n 1 info.txt | sed 's/ //g'`
 echo $pre_title
-post_title=`xmllint --html --xpath "//div[@data-qa='product-card-0']//@aria-label" out.html 2>/dev/null | sed 's/^.*"\(.*\)".*$/\1/'`
+post_title=`xmllint --html --xpath "//div[@data-qa='product-card-0']//@aria-label" out.html 2>/dev/null | sed 's/^.*"\(.*\)".*$/\1/' | sed 's/ //g'`
 echo $post_title
 
-if [ `echo ${pre_title} | sed 's/ *$//g'` = `echo ${post_title} | sed 's/ *$//g'` ]; then
+if [ "$pre_title" = "$post_title" ]; then
 echo "::set-output name=changed::false"
 echo "same name"
 else
